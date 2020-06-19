199e55ca8138c595b5ae389b660a75f9a3974b6e
qdr123 <tmxjdydrb12@gmail.com>
2020-06-19 17:30:59 +0900

    맵 생성및 플레이어
    -플레이어 이동
    -플레이어 카메라 회전

---
+++ New Unity Project (2)/Assets/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float Rspeed = 150;
+    float angleX;
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+        private void Rotate()
+        {
+            float h = Input.GetAxis("Mouse X");
+            angleX += h * Rspeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, angleX, 0);
+        }
+    }
